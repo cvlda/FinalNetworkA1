455374b9e2b4314cb72fb4d74c792324969ece59
johanna <johannak12@gmail.com>
2018-03-25 16:08:01 +0200

    Add total flow table function

--- MixFlowProblem.m
+++ MixFlowProblem.m
@@ -10,7 +10,7 @@
 clear all; close all; clc;
 
 load('Data_prob2.mat')
-% load('test_prob2.mat')
+%load('test_prob2.mat')
 
 %_______________RPM________________________________________________________
 %Decision variables t^0_1, t^0_2, ..., (t^1_1, t^1_2 ....)
@@ -30,6 +30,7 @@ lb = zeros(numdv,1);
 ub = []; 
 
 [x,fval,exitflag,output,lambda] = linprog(obj,Aineq,rhs,[],[],lb,[]); 
+
 pi = lambda.ineqlin;
 sigma  = - lambda.eqlin;
 if min(size(sigma))==0
@@ -50,8 +51,7 @@ while Opt_col==0 || Opt_row==0
     while Opt_col == 0
                    
         % Solve pricing problem
-        [Opt_col, v_addcol] = AddColumns(dv, recap_rate, num_recap,  delta, pi, sigma);
-                
+        [Opt_col, v_addcol] = AddColumns(dv, recap_rate, num_recap,  delta, pi, sigma);        
          if Opt_col==1
              break;
          else
@@ -60,15 +60,13 @@ while Opt_col==0 || Opt_row==0
     
         % Column generation
         [dv, FVAL, pi, sigma] = SolveRPM2(dv,recap_rate,delta,num_flights,num_it,capacity,it, v_addcol, v_addrow);
-           
     end
     
     
     while Opt_row == 0                                 
     
         % Solve separation problem
-         [Opt_row, v_addrow] = AddRows(v_addrow, dv, it, recap_rate, num_it);
-                  
+         [Opt_row, v_addrow] = AddRows(v_addrow, dv, it, recap_rate, num_it);       
          if Opt_row==1
              break;
          else
@@ -78,11 +76,20 @@ while Opt_col==0 || Opt_row==0
         % Row generation
         [dv, FVAL, pi, sigma] = SolveRPM2(dv,recap_rate,delta,num_flights,num_it,...
                                           capacity,it, v_addcol, v_addrow);
-            end
-     
+    end
+        
     
 end
 
 % Results
 %--------------------------------------------------------------------------
 fprintf('Objective Function:    %6f\n',FVAL)
+
+%print flow table for flights between AEP and EZE 
+print_flow_table(dv,delta,it,recap_rate,x_Flight_txt)
+
+%print flow table per itinerary for flights between AEP and EZE 
+
+
+
+
